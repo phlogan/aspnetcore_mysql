e553b813b66ea21e391b88eea3035d8baf7f79ab
phlogan <pheliplogann@gmail.com>
2020-06-22 21:57:26 -0300

    Starting the project

---
+++ ASPMYSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPMYSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
